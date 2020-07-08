04203401730cc883f2f4b68e3160dc36690f8d30
Ahmed Emad <ahmed39652003@gmail.com>
2020-07-08 19:07:41 +0200

    Added Signup and login endpoints

--- app/src/main/java/com/bitnews/bitnews/data/network/APIEndpoints.java
+++ app/src/main/java/com/bitnews/bitnews/data/network/APIEndpoints.java
@@ -1,6 +1,24 @@
 package com.bitnews.bitnews.data.network;
 
+import com.bitnews.bitnews.data.models.AuthToken;
+import com.bitnews.bitnews.data.models.User;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface APIEndpoints {
 
+    @FormUrlEncoded
+    @POST
+    public Call<User> signUp(@Field("first_name") String firstName,
+                             @Field("last_name") String lastName,
+                             @Field("username") String userName,
+                             @Field("password") String password);
 
+    @FormUrlEncoded
+    @POST
+    public Call<AuthToken> logIn(@Field("username") String username,
+                                 @Field("password") String password);
 }
