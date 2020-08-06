5c03c975a7b546c775b86d52958c54a59cb59641
Ahmed Emad <ahmed39652003@gmail.com>
2020-08-07 01:53:27 +0200

    Bug fixes

--- app/src/main/java/com/bitnews/bitnews/ui/activities/SearchActivity.java
+++ app/src/main/java/com/bitnews/bitnews/ui/activities/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.bitnews.bitnews.ui.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bitnews.bitnews.R;
@@ -12,5 +14,17 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        setTitle(R.string.search);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
