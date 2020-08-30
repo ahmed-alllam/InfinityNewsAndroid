8a78ae6eef79a2286e2226c72457722e088a53e3
Ahmed Emad <ahmed39652003@gmail.com>
2020-08-30 15:22:46 +0200

    Added onFinished CallBack to SplashScreen

--- app/src/main/java/com/infinitynews/infinitynews/ui/activities/SplashActivity.java
+++ app/src/main/java/com/infinitynews/infinitynews/ui/activities/SplashActivity.java
@@ -3,7 +3,6 @@ package com.infinitynews.infinitynews.ui.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -12,20 +11,33 @@ import com.infinitynews.infinitynews.R;
 import com.infinitynews.infinitynews.ui.viewmodels.CategoryViewModel;
 import com.infinitynews.infinitynews.ui.viewmodels.UserViewModel;
 
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 public class SplashActivity extends AppCompatActivity {
-    public static final int TIME_TO_WAIT = 2500;
+    private Class<? extends Activity> nextActivityClass;
+    private boolean isAnimationFinished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        GifImageView gifImageView = findViewById(R.id.splashScreen);
+        GifDrawable gifDrawable = (GifDrawable) gifImageView.getDrawable();
+        gifDrawable.addAnimationListener(n -> {
+            isAnimationFinished = true;
+
+            if (nextActivityClass != null)
+                startActivity(nextActivityClass);
+        });
+
         CategoryViewModel categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
         categoryViewModel.hasFavouriteCategories(getApplicationContext()).observe(this, hasFavourites -> {
             if (!hasFavourites)
                 checkIfAuthenticated();
             else
-                addStartActivityDelayToHandler(MainActivity.class);
+                setNextActivity(MainActivity.class);
         });
     }
 
@@ -33,17 +45,22 @@ public class SplashActivity extends AppCompatActivity {
         UserViewModel userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         userViewModel.isUserAuthenticated(getApplicationContext()).observe(this, (isAuthenticated) -> {
             if (isAuthenticated) {
-                addStartActivityDelayToHandler(ChooseCategoriesActivity.class);
+                setNextActivity(ChooseCategoriesActivity.class);
             } else
-                addStartActivityDelayToHandler(TutorialActivity.class);
+                setNextActivity(TutorialActivity.class);
         });
     }
 
-    private void addStartActivityDelayToHandler(Class<? extends Activity> nextActivityClass) {
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, nextActivityClass);
-            startActivity(intent);
-            finish();
-        }, TIME_TO_WAIT);
+    private void setNextActivity(Class<? extends Activity> nextActivityClass) {
+        if (isAnimationFinished)
+            startActivity(nextActivityClass);
+        else
+            this.nextActivityClass = nextActivityClass;
+    }
+
+    private void startActivity(Class<? extends Activity> nextActivityClass) {
+        Intent intent = new Intent(this, nextActivityClass);
+        startActivity(intent);
+        finish();
     }
 }
